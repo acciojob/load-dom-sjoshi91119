9090aef383474675bd62799f4dbb570e4a09b0bf
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 00:39:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  // DOM is loaded and ready to be manipulated here
+  const pTag = document.createElement("p");
+  const textNode = document.createTextNode("DOM load success");
+  pTag.appendChild(textNode);
+  document.body.appendChild(pTag);
+});
